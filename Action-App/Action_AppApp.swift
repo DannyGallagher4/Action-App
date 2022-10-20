326d4a0150c0cac6335c5e9d7c4bf5f0e8943377
Danny Gallagher <dgallagher24@nobles.edu>
2022-10-19 22:59:49 -0400

    Initial Commit

---
+++ Action-App/Action_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Action_AppApp.swift
+//  Action-App
+//
+//  Created by Danny Gallagher on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Action_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
